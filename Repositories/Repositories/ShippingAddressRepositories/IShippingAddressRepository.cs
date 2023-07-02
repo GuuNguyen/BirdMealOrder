87f5331e1ed35d5457c5a1e53d2d1bc41058f685
GuuNguyem <guuhighschool@gmail.com>
2023-07-02 18:49:39 +0700

    shipping address

---
+++ Repositories/Repositories/ShippingAddressRepositories/IShippingAddressRepository.cs
@@ -0,0 +1,18 @@
+﻿using Repositories.DTOs.ShippingAddressDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Repositories.ShippingAddressRepositories
+{
+    public interface IShippingAddressRepository
+    {
+        List<GetSADTO> GetAllByUserId(int userId);
+        GetSADTO GetById(int id);
+        bool CreateSA(CreateSADTO createSADTO);
+        bool UpdateSA(UpdateSADTO updateSADTO);
+        bool DeleteSA(int id);
+    }
+}
--- WebClient/wwwroot/js/ajax-functions.js
+++ WebClient/wwwroot/js/ajax-functions.js
@@ -37,3 +37,56 @@ function addToCart(itemCode, quantity) {
         }
     });
 }
+
+
+ $(document).ready(function () {
+
+        $('#citySelect').change(function () {
+            var cityCodeName = $(this).val();
+            $.ajax({
+                url: "/ShippingAddress/GetDistricts",
+                type: 'GET',
+                data: { cityCodeName: cityCodeName },
+                success: function (result) {
+                    var districtSelect = $('#districtSelect');
+                    districtSelect.empty();
+                    $.each(result, function (i, district) {
+                        districtSelect.append($('<option></option>').val(district.Codename).text(district.Name));
+                    });
+                    districtSelect.change(); 
+                }
+            });
+        });
+
+        $('#districtSelect').change(function () {
+            var districtCodeName = $(this).val();
+            $.ajax({
+                url: "/ShippingAddress/GetWards",
+                type: 'GET',
+                data: { districtCodeName: districtCodeName },
+                success: function (result) {
+                    var wardSelect = $('#wardSelect');
+                    wardSelect.empty();
+                    $.each(result, function (i, ward) {
+                        wardSelect.append($('<option></option>').val(ward.Codename).text(ward.Name));
+                    });
+                }
+            });
+        });
+ });
+
+$(document).ready(function () {
+    $("#addNewDelivery").click(function () {
+        $.ajax({
+            url: "/ShippingAddress/_DeliveryModal",
+            type: "GET",
+            success: function (data) {
+                $("#deliveryModal").html(data);
+                // Hiển thị modal ở đây nếu cần
+            },
+            error: function () {
+                // Xử lý lỗi nếu cần
+            }
+        });
+    });
+});
